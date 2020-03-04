21272062b57471b5729fd206307afa23b631ebec
jmelfers <esjuanma@hotmail.com>
2020-03-04 17:04:59 -0300

    First commit! Tipos de variables y functions

---
+++ basics.py
@@ -0,0 +1,40 @@
+"""
+. Esto lo hizo
+. Martín, Bri y Juanma
+. El 4 de marzo 2020
+. @copyright
+"""
+
+# Comentario
+# de linea
+
+"""
+Bloque de
+comentario
+"""
+
+myVar = 10
+# ar myVar = 10;
+
+# Primitivas
+myVar = 10
+myVar = 'Hello world'
+MY_CONSTANT = True
+
+# Tipos de datos complejos
+myList = [10, 25, 50]
+myList[0] # 10
+myList[1] # 25
+
+myDictionary = {}
+myDictionary['Alumno1'] = 'Pepito' # key = 'Alumno1' y value = 'Pepito'
+myDictionary['Alumno1'] # Pepito
+
+# Javascript -> Objeto plano / plain object
+# PHP -> Array asociativo / associative array
+
+print(myVar) # Hello World
+print(MY_CONSTANT) # True
+
+# Win + R
+# Para ejecutar el codigo desde cmd --> python NombreArchivo.py
---
+++ functions.py
@@ -0,0 +1,12 @@
+def message ():
+	print('Hola! Todo bien?')
+
+def sumar (num1, num2):
+  value = num1 + num2
+  print(value)
+  return value
+  
+message()
+sumar(5, 7)
+
+print('Afuera de la función')
