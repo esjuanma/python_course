a95dcd8e5f43960c0c999cf2d8937cae01e7d32d
jmelfers <esjuanma@hotmail.com>
2020-03-09 17:44:38 -0300

    Agregado de ejercicios

--- homework/1.py
+++ homework/1.py
@@ -1,7 +1,16 @@
+# 1
 # Pedir números al usuario de forma indefinida e imprimir la suma al final
 # para cortar con el pedido de números, utiilzar algún caracter o valor específico
 
 
+# 2
 # Crear función que devuelve las tablas del número 1 al 10
-# ej: [[1,2,3,4,5,6,...],...,[10,20,30,...]]
+# ej: [[1,2,3,4,5,6,...,10], ..., [10,20,30,...]]
 
+
+# 3
+# Pedir al usuario un texto (una o varias palabras) y indicar si es capicua. Case-insensitive.
+
+
+# 4
+# Pedir un número natural al usuario e indicar si es primo. 1 no es primo.
