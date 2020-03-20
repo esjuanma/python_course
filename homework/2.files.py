ef5b0aac522b6ded6d6f7f2332d0ef89e274319b
jmelfers <esjuanma@hotmail.com>
2020-03-20 11:11:32 -0300

    Ejercicio de manipulación de archivos

---
+++ homework/2.files.py
@@ -0,0 +1,12 @@
+# 1
+# Crear un archivo vacío llamado "pow.txt" en el mismo directorio del script (se puede crear de forma "manual")
+# Crear una función en Python que a cada ejecución:
+# - Abra el archivo
+# -- Si el archivo está vacío, escribe un número aleatorio entre 2 y 9 (*) y finaliza el script
+# -- Caso contrario, guarda en memoria el primer y último número encontrado en el archivo,
+# -- y luego escribe, al final del mismo, el producto de ambos números. Finaliza el script.
+
+### AYUDA
+# Revisar exercises/first/expenditures/functions.py para ver manejo de archivos
+# Para abrir un archivo con VSCode, apretar Ctrl+P y escribir el nombre del archivo
+# (*) https://www.programiz.com/python-programming/examples/random-number
