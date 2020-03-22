7c6c14eabebfed27bcf773ca07d5319dff9be384
jmelfers <esjuanma@hotmail.com>
2020-03-22 18:21:50 -0300

    Agregado de server Flask básico

---
+++ server/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+totalScore = 0
+
+@app.route('/home')
+def home():
+    global totalScore
+    score = request.args.get('score')
+
+    if score is not None:
+        score = int(score)
+        totalScore = totalScore + score
+
+    data = {
+        'firstname': 'Jon',
+        'lastname': 'Snow',
+        'score': totalScore
+    }
+
+    # return app.send_static_file('index.html')
+    return render_template('index.html', data = data)
+
+if __name__ == "__main__":
+    app.run(debug = True)
