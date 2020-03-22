066c8269c3738d5824d210b491eb3ea61f1f7855
jmelfers <esjuanma@hotmail.com>
2020-03-22 04:40:41 -0300

    Agregado de ejercicios con librerías combinado con listas

---
+++ homework/3.py
@@ -0,0 +1,82 @@
+monthsTranslation = {
+    'Enero': 'January',
+    'Febrero': 'February',
+    'Marzo': 'March',
+    'Abril': 'April',
+    'Mayo': 'May',
+    'Junio': 'June',
+    'Julio': 'July',
+    'Agosto': 'August',
+    'Septiembre': 'September',
+    'Octubre': 'October',
+    'Noviembre': 'November',
+    'Diciembre': 'December'
+}
+
+# 0
+# Dada la librería monthsTranslation, imprimir, sin usar las palabras February ni November, el siguiente texto:
+# - November Rain was launched by Guns N' Roses on February 18, 1992
+# Dificultad: fácil
+
+
+
+students = [{
+    'firstName': 'Jon',
+    'lastName': 'Snow',
+    'age': 30,
+    'grade': 6
+}, {
+    'firstName': 'Arya',
+    'lastName': 'Stark',
+    'age': 14,
+    'grade': 10
+}]
+
+# 1
+# Se tiene una lista students con 2 librerías representando a 2 alumnos distintos
+# Se pide crear un nuevo alumno en una variable llamada "newStudent"
+# utlizando la misma estructura de los 2 alumnos existentes (variable de tipo librería)
+# (nombre, apellido, edad y nota, con valores arbitrarios a elegir)
+# Dificultad: media-baja
+
+
+# 2
+# Se pide agregar el nuevo alumno a la lista students
+# Dificultad: Muy fácil
+
+
+# 3
+# Dado el listado, ahora con 3 alumnos, se pide:
+# - Calcular e imprimir la edad promedio de los estudiantes
+# - Calcular e imprimir la calificación (grade) promedio de los estudiantes
+# Dificultad: media-alta
+
+
+
+store = {
+    'address': 'Calle falsa 123',
+    'phone': '0303456',
+    'salesByMonth': [
+        15000,
+        10000,
+        14000,
+        17000,
+        9000,
+        20000,
+        19000,
+        22000,
+        24000,
+        21000,
+        5000,
+        11000
+    ]
+}
+
+# 4
+# Dada la librería store, que representa los datos de una determinada tienda, se pide:
+# - Calcular el total de ventas del año (salesByMonth contiene 12 valores representando a cada mes)
+# - Crear un nuevo atributo "totalSales" (key, también conocido como índice) en store
+#   y asignarle como valor, el total calculado previamente
+# Es decir, la librería que actualmente contiene 3 pares key/value, pasará a tener 4 pares
+# una vez que se añada el valor total de ventas
+# Dificultad: media
