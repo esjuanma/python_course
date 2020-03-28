ff9e1ece9b1a154832e0a4352e30bfb212b2b4ac
Brian <bcharlin11@gmail.com>
2020-03-28 20:55:01 -0300

    Coronavirus fix

---
+++ clases/7/coronavirus_algorithm.py
@@ -0,0 +1,49 @@
+# Creates initial variables values
+bodyTemperature = 39.5
+difficultyBreathing = False
+diabetes = True
+cancer = False
+isPregnant = False
+isOver60yearOld = False
+hepatic = False
+kidneyDisease = False
+respiratoryDisease = False
+
+"""
+# Version 1
+if bodyTemperature >= 38:
+    if (difficultyBreathing or
+        diabetes or
+        cancer or
+        isPregnant or
+        isOver60yearOld or
+        hepatic or
+        kidneyDisease or
+        respiratoryDisease):
+        # Prints te vas a morir de coronita
+        print('Tiene coronita')
+    else:
+        # Prints tiene fiebre
+        print('Tiene fiebre')
+else:
+    # Prints ta todo bien
+    print('Diagnostico bueno')
+"""
+# Version 2
+if (bodyTemperature >= 38 and 
+    (difficultyBreathing or
+    diabetes or
+    cancer or
+    isPregnant or
+    isOver60yearOld or
+    hepatic or
+    kidneyDisease or
+    respiratoryDisease)):
+    # Prints te vas a morir de coronita
+    print('Tiene un factor de riesgo')
+elif bodyTemperature >= 38:
+    # Prints tiene fiebre
+    print('Tiene fiebre')
+else:
+    # Prints ta todo bien
+    print('Diagnostico bueno')
